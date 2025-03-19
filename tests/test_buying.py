860cc11bb5109d1d6e06ca2c22d9739008281e15
ilsempo <julian.calvert@outlook.com>
2025-03-19 00:22:56 -0300

    adding new scenario for buying process

--- tests/test_buying.py
+++ tests/test_buying.py
@@ -1,5 +1,5 @@
 import pytest
-from steps.action_steps import login, add_products_to_cart, start_checkout, fill_form_with_data, finish_checkout
+from steps.action_steps import login, add_products_to_cart, start_checkout, fill_form_with_data, finish_checkout, user_clicks
 from steps.navigation_steps import go_to_cart
 from steps.validation_steps import message_is_displayed, user_is_in_page
 
@@ -39,3 +39,20 @@ class TestBuying:
         }, "problem")
         finish_checkout(driver, "problem")
         message_is_displayed(driver, "last_name_missing")
+
+    @pytest.mark.cancel_buying
+    def test_cancel_buying(self, driver):
+        """
+        Scenario: User cancels buying process
+        """
+        login(driver)
+        add_products_to_cart(driver)
+        go_to_cart(driver)
+        start_checkout(driver)
+        fill_form_with_data(driver, {
+        "first_name_input": "first_name",
+        "last_name_input" : "last_name",
+        "postal_code_input" : "postal_code"
+        })
+        user_clicks(driver, "CheckoutTwo", "cancel_button")
+        user_is_in_page(driver, "Cart")
