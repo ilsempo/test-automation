0a3f72b7d0251ad0a7bc81722eed2b74281d21e9
ilsempo <julian.calvert@outlook.com>
2025-06-14 13:54:22 -0300

    add footer coverage

--- tests/test_footer.py
+++ tests/test_footer.py
@@ -1,3 +1,11 @@
 import pytest
 from steps.navigation_steps import login
 from steps.action_steps import logout, user_clicks
+
+#FIXME
+@pytest.mark.footer
+class TestFooterLinks:
+
+    @pytest.mark.footer_links
+    def test_footer_links(self, driver):
+        login(driver)
